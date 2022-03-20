fb93b1ae94dff4a790f074fac3f4cc4feb5a251a
matlidija <matlidija1985@gmail.com>
2022-03-20 14:16:41 +0100

    edit test file

---
+++ Ct_shopTest.java
@@ -0,0 +1,20 @@
+package test;
+
+import base.BaseTest;
+import org.junit.Before;
+import org.junit.Test;
+import page.Ct_shopHome;
+
+public class Ct_shopTest extends BaseTest {
+    Ct_shopHome ct_shopHome;
+
+
+    @Before
+    public void setUpTest(){
+        ct_shopHome = new Ct_shopHome();
+    }
+    @Test
+    public void ct_shopTest(){
+
+    }
+}
